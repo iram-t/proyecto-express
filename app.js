d1e5318b334572ea1798815975b969914c8b7b17
iram-t <iramgustavo235@gmail.com>
2023-04-28 09:48:35 -0600

    express con bodyparser

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const port = 3002;
+//permite el manejo de POST y POST
+const bodyParser = require('body-parser');
+const routes = requiere('./routes/routes');
+const app = express();
 
-app.get('/', function(req, res){
-    res.send('Una API basica desde Express');
-});
+//Usar Node.js body parsing middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true,
+}));
 
-app.get('/saludo', function(req, res){
-    res.send('Hola desde la AIP');
-});
+routes(app);
 
-app.get('/despedida', function(req, res){
-    res.send('Adios desde la API');
-});
-
-app.listen(3000, function(){
-    console.log('Aplicacion ejemplo escuchando en el puerto 3000');
+//iniciar servidor
+const server = app.listen(port, (error) => {
+    if(error) return console.log(`Error: ${error}`);
+    console.log(`El servidor escucha en el puerto ${server.address().port}`);
 });
