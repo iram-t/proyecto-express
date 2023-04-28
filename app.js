961fcd1aa524e0517aa42f96883ae37bcca7d141
iram-t <iramgustavo235@gmail.com>
2023-04-28 09:28:05 -0600

    manejo de rutas

--- app.js
+++ app.js
@@ -2,7 +2,15 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res){
-    res.send('Hola mundo!');
+    res.send('Una API basica desde Express');
+});
+
+app.get('/saludo', function(req, res){
+    res.send('Hola desde la AIP');
+});
+
+app.get('/despedida', function(req, res){
+    res.send('Adios desde la API');
 });
 
 app.listen(3000, function(){
