b3f3525b2c0b24107739abc0cd154b0559d55f2e
iram-t <iramgustavo235@gmail.com>
2023-05-08 09:22:07 -0600

    se agrego metodo get id y post

--- routes/routes.js
+++ routes/routes.js
@@ -15,7 +15,22 @@ const routes = app => {
             response.send(result);
         });
     });
+    app.get('/users/:id', (request, response) => {
+        const id = request.params.id;
+    
+        pool.query('SELECT * FROM users Where id = ?', id, (err, res) => {
+            response.send(res);
+        })
+    });
+
+    app.post('/users', (request, response) =>{ //Instalar postman
+        pool.query('INSERT INTO users SET ?', request.body, (error, result) =>{
+            if (error) throw error;
+            response.status(201).send(`User added wid ID: ${result.insertID}`);
+        });
+    });
 }
 module.exports = routes;
+
 
 //crear la base de datos api y poner el nombre de la tabla users en MySQL
